856e142ae1684b8e52fa45eedf4b933cfa7208e4
Enrique Jimenez <enrique.jimenez@bosonit.com>
2022-05-25 10:59:51 +0200

    Initial commit

---
+++ src/main/java/BS/Prprts/Bs4Application.java
@@ -0,0 +1,13 @@
+package BS.Prprts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs4Application.class, args);
+	}
+
+}
